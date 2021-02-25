caadabfcca22afb4dcccfbc9a13e5090de571ece
ya-sho <78021007+ya-sho@users.noreply.github.com>
2021-02-25 17:37:08 +0900

    商品一覧機能表示1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,13 +2,13 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    #@items=Item.all
+    @items = Item.order('created_at DESC')
   end
 
-  def new 
-    @item=Item.new 
-  end 
-  
+  def new
+    @item = Item.new
+  end
+
   def create
     @item = Item.create(item_params)
     if @item.save
@@ -21,7 +21,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image,:name,:explanation,:category_id,:status_id,:cost_id,:area_id,:days_id,:price).merge(user_id:current_user.id)
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :status_id, :cost_id, :area_id, :days_id,
+                                 :price).merge(user_id: current_user.id)
   end
-
 end
