73490c6c86561a9951d87af5483bf069574dc42a
ya-sho <78021007+ya-sho@users.noreply.github.com>
2021-02-26 18:50:21 +0900

    商品削除機能1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,6 +34,13 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item=Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
+
+
   private
 
   def item_params
